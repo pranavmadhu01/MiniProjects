f953ef017ec184ea1c38bf9c36a01c8baf9066ae
pranav madhu <pranavmadhu73@gmail.com>
2022-07-23 16:00:06 +0530

    deleted react todo app

--- react_to_do_app/src/components/todocards/Todocards.js
+++
@@ -1,9 +0,0 @@
-import "./Todocards.css"
-const Todocards = ({todo}) =>{
-  return(
-    <div>
-      <h4 className={todo.complete?"strike":""}>{todo.task}</h4>
-    </div>
-  )
-}
-export default Todocards;
