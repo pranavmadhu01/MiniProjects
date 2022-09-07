adcb8f0bae5566ca0c98bf470b7d63a0d88033c4
pranav madhu <pranavmadhu73@gmail.com>
2022-09-07 13:46:19 +0530

    setup a download button for image and host name detection

--- qrcode_generator/src/App.js
+++ qrcode_generator/src/App.js
@@ -5,13 +5,27 @@ var QRCode = require("qrcode");
 function App() {
   const [Url, setUrl] = useState("");
   const [qrcode, setQrcode] = useState("");
+  const [hostname, setHostname] = useState("");
   function GenerateQR() {
     QRCode.toDataURL("I am a pony!", function (err, url) {
-      if(err){
-        console.error(err)
-      }
-      else{
-        setQrcode(url)
+      if (err) {
+        console.error(err);
+      } else {
+        var url3 = Url;
+
+        var j = url3.indexOf("://");
+
+        var host = "";
+
+        for (var i = j + 3; i < url3.length; i++) {
+          if (url3.charAt(i) != "/") {
+            host = host + "" + url3.charAt(i);
+          } else {
+            break;
+          }
+        }
+        setHostname(host);
+        setQrcode(url);
       }
     });
   }
@@ -21,13 +35,16 @@ function App() {
       <div>
         <input
           type="text"
-          placeholder="Generate your Qr code for free"
+          placeholder="eg. https://github.com/"
           value={Url}
           onChange={(e) => setUrl(e.target.value)}
         />
         <button onClick={GenerateQR}>Generate</button>
-        <img src={qrcode} alt="" />
       </div>
+      <img src={qrcode} alt="" />
+      <a href={qrcode} download={`${hostname}.png`}>
+        <button>Download here</button>
+      </a>
     </div>
   );
 }
